d73cfabeba070553403de199d41f11be1cd285f7
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-08-17 15:20:01 +0530

    add: routes

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -3,6 +3,28 @@ import pokeballIcon from "../assets/pokeball-icon.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const navigationRoutes = [
+    {
+      name: "Search",
+      route: "/search",
+    },
+    {
+      name: "Compare",
+      route: "/compare",
+    },
+    {
+      name: "Pokemon",
+      route: "/pokemon",
+    },
+    {
+      name: "My List",
+      route: "/list",
+    },
+    {
+      name: "About",
+      route: "/about",
+    },
+  ];
   return (
     <nav>
       <div className="block">

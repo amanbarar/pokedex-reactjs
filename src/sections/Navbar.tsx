87974d425df1e3964020b92227dc2848e89aad45
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-08-18 16:14:14 +0530

    add: navbar effects

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import pokeballIcon from "../assets/pokeball-icon.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const location = useLocation();
   const navigationRoutes = [
     {
       name: "Search",
@@ -26,6 +27,21 @@ function Navbar() {
       route: "/about",
     },
   ];
+
+  useEffect(() => {
+    const index = navigationRoutes.findIndex(({route})=>
+    location.pathname.includes(route)
+    );
+    ul(index)
+  })
+
+  function ul(index:number) {
+    const underlines = document.querySelectorAll<HTMLElement>(".underline")
+    for(let i = 0; i < underlines.length; i++) {
+      underlines[i].style.transform = "translate3d(" + index * 100 +"%,0,0)";
+    }
+  }
+
   return (
     <nav>
       <div className="block">
@@ -33,6 +49,9 @@ function Navbar() {
       </div>
       <div className="data">
         <ul>
+          <div className="underline"></div>
+          <div className="underline"></div>
+          <div className="underline"></div>
           {navigationRoutes.map(({ name, route }, index) => {
             return (
               <Link to={route} key={index}>

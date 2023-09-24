f6a0bbc0e14f6c89a1acd2b958e37b07028aa3db
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-09-24 19:34:59 +0530

    add: extra reducers

--- src/app/slices/PokemonSlice.ts
+++ src/app/slices/PokemonSlice.ts
@@ -1,6 +1,7 @@
 // pokemon data
 import { createSlice } from "@reduxjs/toolkit";
 import { PokemonTypeInitialState } from "../../utils/Types";
+import { getInitialPokemonData } from "../reducers/getInitialPokemonData";
 
 const initialState: PokemonTypeInitialState = {
   allPokemon: undefined,
@@ -10,6 +11,11 @@ export const PokemonSlice = createSlice({
   name:"app",
   initialState,
   reducers:{},
+  extraReducers: (builder) => {
+    builder.addCase(getInitialPokemonData.fulfilled, (state, action) => {
+      state.allPokemon = action.payload
+    });
+  },
 });
 
 export const {} = PokemonSlice.actions;

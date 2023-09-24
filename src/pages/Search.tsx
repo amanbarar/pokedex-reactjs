bc337462b16135966999e86187d8dbd66ac244c0
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-09-24 19:54:49 +0530

    add: interface generatedPokemonType in Types.ts

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
+import { getPokemonData } from '../app/reducers/getPokemonData';
 
 function Search() {
   const dispatch = useAppDispatch();    //calling reducers
@@ -16,8 +17,9 @@ function Search() {
       const randomPokemonsId = clonedPokemons
         .sort(() => Math.random() - Math.random())
         .slice(0, 20);
+        dispatch(getPokemonData(randomPokemonsId))
     }
-  }, [allPokemon])
+  }, [allPokemon, dispatch])
   return (
     <div>Search</div>
   )

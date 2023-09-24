db568276d0e3d83dd6c70165c0a97fd020e9ea2f
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-09-24 19:41:46 +0530

    generate random pokemons on search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper';
-import { useAppDispatch } from '../app/hooks';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
 
 function Search() {
   const dispatch = useAppDispatch();    //calling reducers
+  const { allPokemon } = useAppSelector(({ pokemon }) => pokemon)
   useEffect(() => {
     dispatch(getInitialPokemonData());
   }, [dispatch]);
+
+  useEffect(() => {
+    if(allPokemon) {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+        console.log(randomPokemonsId);
+    }
+  }, [allPokemon])
   return (
     <div>Search</div>
   )

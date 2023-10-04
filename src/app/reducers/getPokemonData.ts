c2331a21de0f6a2c872c0cf62d425a10505f9245
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-10-04 18:50:15 +0530

    add: getPokemonTypes.ts

--- src/app/reducers/getPokemonData.ts
+++ src/app/reducers/getPokemonData.ts
@@ -3,12 +3,36 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { generatedPokemonType, genericPokemonType } from "../../utils/Types";
 import axios from "axios";
+import { defaultImages, images } from "../../utils/getPokemonImages";
 
 export const getPokemonData = createAsyncThunk("pokemon/randomPokemon", async (pokemons:genericPokemonType[]) => {
   try {
     const pokemonsData: generatedPokemonType[] = [];
     for await (const pokemon of pokemons) {
-      const {data} = await axios.get(pokemon.url);
+      const {data}: {
+        data: {
+          id: number;
+          types: {type: generatedPokemonType}[];
+        };
+      } = await axios.get(pokemon.url);
+      const types = data.types.map(({type: {name}}: {type: {name: string}}) => {
+        [name]: pokemonTypes[name]
+      })
+
+      // @ts-expect-error
+      let image: string = images[data.id]
+      if (!image) {
+        // @ts-expect-error
+        image = defaultImages[data.id];
+      }
+      if (image) {
+        pokemonsData.push({
+          name: [pokemon.name],
+          id: data.id,
+          image,
+          types,
+        })
+      }
       console.log({data});
     }
   } catch (err) {
